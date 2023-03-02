c781f515ee494b5602fdec4d89af074182fd920d
devproo <ipeerless0@gmail.com>
2023-03-02 13:57:38 +0200

    Initial Commit

---
+++ movies-booking/movies_bookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  movies_bookingApp.swift
+//  movies-booking
+//
+//  Created by ipeerless on 02/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct movies_bookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
